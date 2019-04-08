f5fe65af395be906f59b4acc68c81b299ed99a4d
Gabriel Frischknecht <gabriel.frischknecht@isolutions.ch>
2019-04-08 10:32:31 +0200

    first working version

---
+++ first-web-application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace first_web_application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
